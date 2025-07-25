c40cb6181aaf733294bea5312b2d0ce803b8d48c
shumbamhinii <168354492+shumbamhinii@users.noreply.github.com>
2025-07-25 08:55:51 +0200

    Update Tasks.tsx

--- src/pages/Tasks.tsx
+++ src/pages/Tasks.tsx
@@ -1,28 +1,7 @@
-import React from 'react';
-import { Header } from '@/components/layout/Header'; // Added Header import
+import React from 'react'; // Keep React import for JSX
+import { Header } from '@/components/layout/Header';
 import { KanbanBoard } from '@/components/tasks/KanbanBoard';
-import { motion } from 'framer-motion'; // Added motion import
-
-const Tasks = () => {
-  return (
-    <div className='flex-1 space-y-4 p-4 md:p-6 lg:p-8'>
-      <Header title='Tasks' /> {/* Added Header component */}
-
-      <motion.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 0.5 }}
-      >
-        <KanbanBoard />
-      </motion.div>
-    </div>
-  );
-};
-
-export default Tasks;
-import { Header } from '@/components/layout/Header'
-import { KanbanBoard } from '@/components/tasks/KanbanBoard'
-import { motion } from 'framer-motion'
+import { motion } from 'framer-motion';
 
 const Tasks = () => {
   return (
@@ -37,7 +16,7 @@ const Tasks = () => {
         <KanbanBoard />
       </motion.div>
     </div>
-  )
-}
+  );
+};
 
-export default Tasks
+export default Tasks;
